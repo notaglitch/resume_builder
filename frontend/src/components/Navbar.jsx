d9a82179df1b4659cc75f6b8b615848735f1e821
Ghafoor <ghafoornazari@proton.me>
2025-02-27 17:13:39 +0100

    Added the frontend files

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-800 text-white px-8 py-6 flex justify-between items-center">
+      <h1 className="text-2xl font-bold cursor-pointer">Resume Builder</h1>
+      <div className="flex justify-end gap-8">
+        <Link to="/" className="text-white hover:text-gray-300">Home</Link>
+        <Link to="/templates" className="text-white hover:text-gray-300">Templates</Link>
+        <Link to="/about" className="text-white hover:text-gray-300">About</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
